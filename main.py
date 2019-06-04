250b8d0aac8d0390a9308a4ce01db9f77dbe67a6
Duco Lindhout <duco.lindhout@gmail.com>
2019-06-04 19:42:28 +0200

    No loops through primes list and adds them to a file. Also deleted open.py

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
-for i in range(1000):
+from tqdm import tqdm
+
+
+primes = []
+
+
+for i in tqdm(range(1000000)):
+
     prime = True
+
     for x in range(i):
         if x == 0 or x == 1 or x == (i+1):
             continue
-        #print("%s / %s" % ((i+1), x))
+
         if (i+1) % (x) == 0: #If remainer is 0 so it is divisable by something.
-            #print("%s is not a prime number" % (1+i))
+
             prime = False
             break
 
     if prime:    
-        print("%s is a prime number" % (1+i))   #If it is not divisible by anything. 
+        primes.append((i+1))   #If it is not divisible by anything. 
 print("done")
+
+f = open("primes.txt", "a+")
+
+for i in primes:
+    f.write(str(i)+"\t")
+
+f.close()
+
     
--- open.py
+++
@@ -1,6 +0,0 @@
-import re
-f = open("primes1.txt", "r")
-
-contents = f.read()
-numbers = contents.split()
-print(numbers)
