dc145dd7d46a1eac968493d1225df81f2f9d297d
Duco Lindhout <duco.lindhout@gmail.com>
2019-06-04 18:45:51 +0200

    Changes were made

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import math
 
-for i in range(9000):
-    if i / i == 0:
-        print("i/i = 0")
+for i in range(100):
+    for x in range(i):
+        if (i+1) % (x+1) == 0:
+            print("%s is not a prime number" % i)
+            break
+        
+    
