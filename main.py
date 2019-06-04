9e08be4c1174963d9073882c5ee0fe4cb70b088c
Duco Lindhout <duco.lindhout@gmail.com>
2019-06-04 19:31:01 +0200

    Added file with primes and added file to open and format prime file

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-import math
-
-for i in range(100):
+for i in range(1000):
+    prime = True
     for x in range(i):
-        if (i+1) % (x+1) == 0:
-            print("%s is not a prime number" % i)
+        if x == 0 or x == 1 or x == (i+1):
+            continue
+        #print("%s / %s" % ((i+1), x))
+        if (i+1) % (x) == 0: #If remainer is 0 so it is divisable by something.
+            #print("%s is not a prime number" % (1+i))
+            prime = False
             break
-        
+
+    if prime:    
+        print("%s is a prime number" % (1+i))   #If it is not divisible by anything. 
+print("done")
     
---
+++ open.py
@@ -0,0 +1,6 @@
+import re
+f = open("primes1.txt", "r")
+
+contents = f.read()
+numbers = contents.split()
+print(numbers)
